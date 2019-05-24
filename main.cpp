2991cf389edbccbcbdd5d3ab8aeac677768d1fc6
Miroslav Houdek <miroslav.houdek@gmail.com>
2019-05-24 15:52:09 +0200

    Initial commit. Contains skeleton of the Sudoku example to accelerate the development, large part of Sudoku related functions is removed to make it simple to navigate, but some are kept and are to be modified for the Einstein riddle example (or replaced by own algorithms later).  Also added .gitignore

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include "solver.hpp"
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <chrono>
+
+int main() {
+    try {
+        auto t1 = std::chrono::high_resolution_clock::now();
+
+        Solver s;
+
+        if (s.solve()) {
+            std::chrono::duration<double, std::milli> time_taken = std::chrono::high_resolution_clock::now() - t1;
+            std::clog << "Solution found in " << time_taken.count() << " ms\n";
+
+            auto solution = s.get_solution();
+            for (auto const& row : solution) {
+                for (auto const& col : row) {
+                    std::cout << col << ' ';
+                }
+                std::cout << '\n';
+            }
+        } else {
+            std::clog << "Solving the provided parsed is not possible\n";
+        }
+    } catch(std::exception const& ex) {
+        std::clog << "Failed parsing because: " << ex.what() << std::endl;
+        return 1;
+    }
+}
